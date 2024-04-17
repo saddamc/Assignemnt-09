6c08c1546a02c2229ba10312ad56e610c9781d4b
saddamcall <saddam13bd@gmail.com>
2024-04-17 16:58:11 +0600

    footer complete

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="mx-auto text-center items-center mt-12 ">
-            <h2 className="text-5xl font-bold mb-6 text-red-500">Oops!!! something went wrong</h2>
-            <p className="text-xl font-light mb-6">Sorry, we couldn't find your page</p>
-            <Link to="/"><button className="btn bg-slate-300  text-2xl font-bold">Back to Homepage</button></Link>
+        <div className="mx-auto min-h-[1600px] font-fira text-center items-center  bg-slate-400 ">
+            <div>
+                <h2 className="text-5xl font-bold mb-6 text-red-500 pt-[200px] ">Oops!!! something went wrong</h2>
+                <p className="text-xl font-light mb-6">Sorry, We couldn't find your page</p>
+                <Link to="/"><button className="btn bg-slate-200 text-2xl font-bold">Back to Homepage</button></Link>
+            </div>
         </div>
     );
 };
